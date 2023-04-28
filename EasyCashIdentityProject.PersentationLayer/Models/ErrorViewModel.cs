fdc690089a873aa55374d91fa95596e1795cfd9d
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-04-29 00:10:05 +0300

    Add project files.

---
+++ EasyCashIdentityProject.PersentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyCashIdentityProject.PersentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
