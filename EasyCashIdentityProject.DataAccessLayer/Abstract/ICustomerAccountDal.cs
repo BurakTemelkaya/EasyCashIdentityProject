2685a151e1a83252a2a9813a54c3c0ff360d13cf
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-05-08 21:50:06 +0300

    Lesson 8

---
+++ EasyCashIdentityProject.DataAccessLayer/Abstract/ICustomerAccountDal.cs
@@ -0,0 +1,14 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Abstract
+{
+    public interface ICustomerAccountDal:IGenericDal<CustomerAccount>
+    {
+
+    }
+}
