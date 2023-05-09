3361d53e7fc7f1eda1c86a25da46cd5a092c6669
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-05-09 20:46:54 +0300

    Lesson 9

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,47 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    
+    public class CustomerAccountProcessManager:ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Delete(t);
+        }
+
+        public List<CustomerAccountProcess> TGetAll()
+        {
+            return _customerAccountProcessDal.GetAll();
+        }
+
+        public CustomerAccountProcess TGetById(int id)
+        {
+            return _customerAccountProcessDal.GetById(id);
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Update(t);
+        }
+    }
+}
---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountServiceManager.cs
@@ -0,0 +1,45 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountServiceManager : ICustomerAccountService
+    {
+        private readonly ICustomerAccountService _customerAccountService;
+
+        public CustomerAccountServiceManager(ICustomerAccountService customerAccountService)
+        {
+            _customerAccountService = customerAccountService;
+        }
+
+        public void TDelete(CustomerAccount t)
+        {
+            _customerAccountService.TDelete(t);
+        }
+
+        public List<CustomerAccount> TGetAll()
+        {
+            return _customerAccountService.TGetAll();
+        }
+
+        public CustomerAccount TGetById(int id)
+        {
+            return _customerAccountService.TGetById(id);
+        }
+
+        public void TInsert(CustomerAccount t)
+        {
+            _customerAccountService.TInsert(t);
+        }
+
+        public void TUpdate(CustomerAccount t)
+        {
+            _customerAccountService.TUpdate(t);
+        }
+    }
+}
