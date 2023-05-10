9d064a98657d420abd884b965eed333703230144
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-05-10 20:30:55 +0300

    Lesson 10

---
+++ EasyCashIdentityProject.PersentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appuser = new()
+                {
+                    UserName = appUserRegisterDto.UserName,
+                    Name = appUserRegisterDto.Name,
+                    SurName = appUserRegisterDto.Surname,
+                    Email = appUserRegisterDto.Email,
+                };
+                var result = await _userManager.CreateAsync(appuser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+            }
+            return View(appUserRegisterDto);
+        }
+    }
+}
